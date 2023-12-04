1dd90ed41249a96828a7fc76ea43e712f6dfcbb6
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-05 03:33:41 +0700

    add fun HostNavigasi

--- app/src/main/java/com/example/roomdatabase/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomdatabase/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,27 @@
 package com.example.roomdatabase.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.roomdatabase.ui.ui.halaman.DestinasiEntry
+import com.example.roomdatabase.ui.ui.halaman.DestinasiHome
+import com.example.roomdatabase.ui.ui.halaman.EntrySiswaScreen
+import com.example.roomdatabase.ui.ui.halaman.HomeScreen
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+    }
+}
