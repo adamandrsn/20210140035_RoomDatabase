359f89aaa1b48bb39cc576b84fa9e0c9db8b1130
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-05 02:32:05 +0700

    add class EntryViewModel

--- app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/EntryViewModel.kt
@@ -1,7 +1,34 @@
 package com.example.roomdatabase.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.data.Siswa
+import com.example.roomdatabase.repositori.RepositoriSiswa
 
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
+    suspend fun saveSiswa(){
+        if(validasiInput()){
+            repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
+        }
+    }
+}
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
