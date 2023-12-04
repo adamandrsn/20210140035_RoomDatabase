6a43246041c6ef930fb41b8ead99f90b1caa6699
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-05 03:35:20 +0700

    add fun SiswaApp

--- app/src/main/java/com/example/roomdatabase/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/roomdatabase/navigasi/PengelolaHalaman.kt
@@ -14,12 +14,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.roomdatabase.R
 import com.example.roomdatabase.ui.ui.halaman.DestinasiEntry
 import com.example.roomdatabase.ui.ui.halaman.DestinasiHome
 import com.example.roomdatabase.ui.ui.halaman.EntrySiswaScreen
 import com.example.roomdatabase.ui.ui.halaman.HomeScreen
 
+@Composable
+fun SiswaApp(navController: NavHostController = rememberNavController()){
+    HostNavigasi(navController = navController)
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
