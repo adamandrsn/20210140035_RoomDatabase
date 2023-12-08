dd4fac2b97adf46c455cba486c6468529a6cf1cd
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-08 09:31:57 +0700

    create class EditViewModel

---
+++ app/src/main/java/com/example/roomdatabase/ui/ui/model/EditViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.roomdatabase.ui.ui.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase.repositori.RepositoriSiswa
+import com.example.roomdatabase.ui.model.UIStateSiswa
+import com.example.roomdatabase.ui.model.toUiStateSiswa
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoriSiswa: RepositoriSiswa
+) : ViewModel() {
+
+    var siswaUiState by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val itemId: Int = checkNotNull(savedStateHandle[ItemEditDestination.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            siswaUiState = repositoriSiswa.getSiswaStream(itemId)
+                .filterNotNull()
+                .first()
+                .toUiStateSiswa(true)
+        }
+    }
+}
