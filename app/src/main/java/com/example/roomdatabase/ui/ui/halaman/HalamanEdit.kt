83ce7eba28f979a2c16c3fd412892656165cdc6b
Adam Anderson Yuniarto <adam.anderson.ft21@mail.umy.ac.id>
2023-12-08 09:25:09 +0700

    create class DetailsViewModel 2

--- app/src/main/java/com/example/roomdatabase/ui/ui/halaman/HalamanEdit.kt
+++ app/src/main/java/com/example/roomdatabase/ui/ui/halaman/HalamanEdit.kt
@@ -2,7 +2,14 @@ package com.example.roomdatabase.ui.ui.halaman
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdatabase.repositori.RepositoriSiswa
+import com.example.roomdatabase.ui.model.toDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailsViewModel(
     savedStateHandle: SavedStateHandle,
@@ -10,4 +17,22 @@ class DetailsViewModel(
 ) : ViewModel(){
 
     private val siswaId: Int = checkNotNull(savedStateHandle[DetailsDestination.siswaIdArg])
+    val uiState : StateFlow<ItemDetailsUiState> =
+        repositoriSiswa.getSiswaStream(siswaId)
+            .filterNotNull()
+            .map {
+                ItemDetailsUiState(detailSiswa = it.toDetailSiswa())
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+                initialValue = ItemDetailsUiState()
+            )
+    suspend fun deleteItem(){
+        repositoriSiswa.deleteSiswa(uiState.value.detailSiswa.toSiswa())
+    }
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
 }
